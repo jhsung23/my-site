6111b357af96a8b792434286eb05cd03db0a3593
sungjihyun <jhsung23@gmail.com>
2023-12-06 18:46:53 +0900

    fix: 컴포넌트 호버 시 헤더를 가리는 문제 해결

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import { Logo, Nav } from '.';
 
 export default function Header() {
   return (
-    <header className="sticky flex items-center justify-between w-11/12 max-w-5xl pt-16 pb-8 bg-primary -top-8 bg-opacity-90 dark:bg-opacity-90">
+    <header className="sticky z-10 flex items-center justify-between w-11/12 max-w-5xl pt-16 pb-8 bg-primary -top-8 bg-opacity-90 dark:bg-opacity-90">
       <Logo />
       <div className="flex items-center gap-4">
         <Nav />
