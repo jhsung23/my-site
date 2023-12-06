a5ca099ba1ff378e45089e42cd5e7b25bfe1f494
sungjihyun <jhsung23@gmail.com>
2023-12-06 21:43:41 +0900

    chore: 내비게이션 메뉴 이름 변경 (posts->blog)

--- src/components/layouts/Nav.tsx
+++ src/components/layouts/Nav.tsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav className="flex select-none">
       <NavLink href="/">About</NavLink>
-      <NavLink href="/posts">Blog</NavLink>
+      <NavLink href="/blog">Blog</NavLink>
       <NavLink href="/snippets">Snippets</NavLink>
       <NavLink href="/projects">Projects</NavLink>
     </nav>
