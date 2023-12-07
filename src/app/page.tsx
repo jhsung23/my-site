be71448e55955b207b41ca87b467737a9d417e9c
sungjihyun <jhsung23@gmail.com>
2023-12-07 16:41:07 +0900

    feat: About 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <></>;
+import { H1, Paragraph } from '@/components/common';
+import { MenuPageIcon } from '@/components/layouts';
+
+export default async function AboutPage() {
+  return (
+    <>
+      <MenuPageIcon>👋</MenuPageIcon>
+      <H1 className="mb-2">Hello</H1>
+      <Paragraph>안녕하세요. FrontEnd 개발자 성지현입니다.</Paragraph>
+    </>
+  );
 }
