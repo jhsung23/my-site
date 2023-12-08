4339c2146b0011bc57ba18e1fd622926ad59a2c0
sungjihyun <jhsung23@gmail.com>
2023-12-08 13:39:26 +0900

    chore: logo png to webp

--- src/components/layouts/Logo.tsx
+++ src/components/layouts/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <Link href="/">
-      <img src="/assets/logo.png" alt="logo" className="w-12 h-12" />
+      <img src="/logo.webp" alt="logo" className="w-12 h-12" />
     </Link>
   );
 }
