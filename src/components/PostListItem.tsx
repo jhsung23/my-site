e87984f9490e685ebc9068e45944059b55689c4b
sungjihyun <jhsung23@gmail.com>
2023-12-12 19:29:16 +0900

    fix: jsx 문법 오류 수정

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -12,8 +12,8 @@ export default function PostListItem({ title, subtitle, slug, tags }: Props) {
       <Link href={`blog/${slug}`} className="flex grow">
         <div className="bg-secondary flex grow flex-col self-stretch rounded-md p-5">
           <H3>{title}</H3>
-          {subtitle.length && <Paragraph className="text-tertiary mt-2">{subtitle}</Paragraph>}
-          {tags.length && (
+          {subtitle.length > 0 && <Paragraph className="text-tertiary mt-2">{subtitle}</Paragraph>}
+          {tags.length > 0 && (
             <div className="mt-2 flex flex-wrap gap-1.5">
               {tags?.map((tag) => <Tag key={tag}>{tag}</Tag>)}
             </div>
