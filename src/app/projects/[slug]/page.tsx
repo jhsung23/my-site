75a89b8cdb296a3bd386ce6add7d0113326b27f3
sungjihyun <jhsung23@gmail.com>
2023-12-13 16:07:38 +0900

    feat: icon 추가 및 Projects 상세 페이지

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,80 @@
+import Image from 'next/image';
+
+import { getAllProjects, getProjectBySlug } from '@/apis/projectService';
+import { CalendarIcon, PersonIcon, RepositoryIcon } from '@/assets/icons';
+import { ReadingProgressBar } from '@/components';
+import { H3 as Description, Paragraph, H1 as Title } from '@/components/common';
+import { Project } from '@/types/project';
+import { parseNotionPageToHtml } from '@/utils/parseContents';
+
+export interface ProjectWithContent extends Project {
+  content: Awaited<ReturnType<typeof parseNotionPageToHtml>>;
+}
+
+const getProjectDataBySlug = async (slug: string): Promise<ProjectWithContent> => {
+  const project = await getProjectBySlug(slug);
+  const content = await parseNotionPageToHtml(project.pageId);
+
+  return { ...project, content };
+};
+
+export async function generateStaticParams() {
+  const projects = await getAllProjects();
+
+  return projects.map((project) => ({
+    params: { slug: project.slug },
+  }));
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { cover, projectTitle, description, startDate, endDate, repository, team, content } =
+    await getProjectDataBySlug(params.slug);
+
+  return (
+    <>
+      <ReadingProgressBar />
+
+      <Title className="mb-3 break-words">{projectTitle}</Title>
+      <Description className="mb-1 mt-3 text-lg font-normal">{description}</Description>
+
+      <hr className="bg-mute mb-10 mt-4 h-0.5 border-0" />
+
+      <div className="flex flex-col md:flex-row md:gap-8">
+        <section className="md:basis-1/3">
+          <Image
+            src={cover}
+            width={400}
+            height={400}
+            alt="thumbnail"
+            priority
+            className="h-auto w-full rounded-md"
+          />
+
+          <div className="mt-3 flex flex-col gap-1.5">
+            <Paragraph className="flex items-center gap-2 font-normal">
+              <PersonIcon width={20} height={20} />
+              {team}
+            </Paragraph>
+            <Paragraph className="flex items-center gap-2 font-normal">
+              <CalendarIcon width={20} height={20} />
+              {startDate} ~ {endDate}
+            </Paragraph>
+            <Paragraph className="flex items-center gap-2 font-normal">
+              <RepositoryIcon width={20} height={20} />
+              <a href={repository} target="_blank" className="hover:text-mute">
+                Github repository 바로가기
+              </a>
+            </Paragraph>
+          </div>
+        </section>
+
+        <hr className="my-5 md:hidden" />
+
+        <article
+          className="prose grow dark:prose-invert md:basis-2/3"
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></article>
+      </div>
+    </>
+  );
+}
