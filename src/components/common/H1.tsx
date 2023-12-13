46762490895109491e807243fa4cb9589f7b9afe
sungjihyun <jhsung23@gmail.com>
2023-12-13 13:55:36 +0900

    design: heading 폰트 크기 변경

--- src/components/common/H1.tsx
+++ src/components/common/H1.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/utils/cn';
 
 export default function H1({ children, className, ...props }: React.ComponentProps<'h1'>) {
   return (
-    <h1 className={cn('font-bold text-primary text-4xl break-all', className)} {...props}>
+    <h1 className={cn('font-bold text-primary text-3xl break-all', className)} {...props}>
       {children}
     </h1>
   );
--- src/components/common/H2.tsx
+++ src/components/common/H2.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/utils/cn';
 
 export default function H2({ children, className, ...props }: React.ComponentProps<'h2'>) {
   return (
-    <h2 className={cn('font-bold text-primary text-3xl break-all', className)} {...props}>
+    <h2 className={cn('font-bold text-primary text-2xl break-all', className)} {...props}>
       {children}
     </h2>
   );
