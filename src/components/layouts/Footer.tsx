f1a2c4dad0a81a78c23858480a70b7e60f4b9dc0
sungjihyun <jhsung23@gmail.com>
2023-12-22 23:08:28 +0900

    chore: footer 문구 변경

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -5,12 +5,12 @@ export default function Footer({ className, ...props }: React.ComponentProps<'fo
   return (
     <footer
       className={cn(
-        'flex flex-col items-center border-t-[0.1px] text-sm w-full text-tertiary border-black-600 mt-24 pb-24 pt-4',
+        'flex flex-col items-center border-t-[0.1px] text-sm w-full text-tertiary border-black-600 mt-24 pb-12 pt-4',
         className,
       )}
       {...props}
     >
-      {`ⓒ 2023-2024 Sungjihyun`}
+      {`ⓒ 2023-2024 Sungjihyun Powered by Next.js`}
       <div className="mt-2 flex gap-4">
         <a href="https://github.com/jhsung23" target="_blank" aria-label="Github">
           <GithubIcon width={'20px'} height={'20px'} />
