0100ecaed8d968b7b8f46e62b8cebcef8168c2ae
sungjihyun <jhsung23@gmail.com>
2023-12-22 15:33:32 +0900

    feat: 페이지별 loading 페이지

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,14 +3,15 @@ export default function Loading() {
     <div className="mt-4 flex flex-col gap-4">
       <div className="bg-secondary h-14 w-14 rounded-md"></div>
       <div className="bg-secondary h-10 w-1/6 rounded"></div>
-      <div className="bg-secondary h-8 w-1/2 rounded"></div>
+      <div className="bg-secondary h-40 w-full rounded"></div>
 
-      <div className="bg-secondary mt-8 h-16 w-full rounded"></div>
-      <div className="bg-secondary h-16 w-full rounded"></div>
-      <div className="bg-secondary h-16 w-full rounded"></div>
-      <div className="bg-secondary h-16 w-full rounded"></div>
-      <div className="bg-secondary h-16 w-full rounded"></div>
-      <div className="bg-secondary h-16 w-full rounded"></div>
+      <div className="bg-secondary mt-12 h-10 w-1/2 rounded"></div>
+      <div className="mt-3 grid grid-cols-2 gap-4 lg:grid-cols-4">
+        <div className="bg-secondary h-24 w-full rounded"></div>
+        <div className="bg-secondary h-24 w-full rounded"></div>
+        <div className="bg-secondary h-24 w-full rounded"></div>
+        <div className="bg-secondary h-24 w-full rounded"></div>
+      </div>
     </div>
   );
 }
