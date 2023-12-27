cd40f8e2bb93a7594314248e2318bb18fd45f3ad
sungjihyun <jhsung23@gmail.com>
2023-12-27 18:07:27 +0900

    feat: 상세 페이지별 loading 페이지

---
+++ src/app/blog/[slug]/loading.tsx
@@ -0,0 +1,21 @@
+export default function loading() {
+  return (
+    <div className="mt-4 flex flex-col gap-4">
+      <div className="bg-secondary h-8 w-16 rounded-md"></div>
+      <div className="bg-secondary h-12 w-full rounded-md"></div>
+      <div className="bg-secondary h-8 w-2/3 rounded"></div>
+
+      <div className="bg-secondary h-1 w-full rounded"></div>
+
+      <div className="bg-secondary mt-12 h-10 w-1/2 rounded"></div>
+      <div className="bg-secondary h-24 rounded"></div>
+      <div className="bg-secondary h-10 w-full rounded"></div>
+      <div className="bg-secondary h-10 w-full rounded"></div>
+
+      <div className="bg-secondary mt-12 h-10 w-1/2 rounded"></div>
+      <div className="bg-secondary h-24 rounded"></div>
+      <div className="bg-secondary h-10 w-full rounded"></div>
+      <div className="bg-secondary h-10 w-full rounded"></div>
+    </div>
+  );
+}
---
+++ src/app/projects/[slug]/loading.tsx
@@ -0,0 +1,16 @@
+export default function loading() {
+  return (
+    <div className="mt-4 flex flex-col gap-4">
+      <div className="bg-secondary h-12 w-1/4 rounded-md"></div>
+      <div className="bg-secondary h-8 w-2/3 rounded"></div>
+
+      <div className="bg-secondary h-1 w-full rounded"></div>
+
+      <div className="bg-secondary mt-12 h-64 rounded"></div>
+
+      <div className="bg-secondary h-10 w-full rounded"></div>
+      <div className="bg-secondary h-10 w-full rounded"></div>
+      <div className="bg-secondary h-10 w-full rounded"></div>
+    </div>
+  );
+}
